1528489f6f93c426fd51955eaab271385617b9a8
Arash Ushani <aushani@gmail.com>
2018-02-08 07:24:56 -0500

    more occ grid

--- manage_occupancy_grid.py
+++ manage_occupancy_grid.py
@@ -46,12 +46,13 @@ class map:
             pose_img[0] = int(self._pose[0]/self._res + self._dim[0]/2)
             pose_img[1] = int(self._pose[1]/self._res + self._dim[1]/2)
 
-            # M = self.get_M(pose_img + [self._pose[2]])
-            scale_factor = 0.1 * self._res
-            M = cv2.getRotationMatrix2D((0, 133), np.rad2deg(self._pose[2]), scale_factor) # includes scale from cm to mm
+            scale_factor = 0.1 / self._res
+
+            rot_y = 300
+            M = cv2.getRotationMatrix2D((0, rot_y), np.rad2deg(self._pose[2]), scale_factor) # includes scale from cm to mm
 
             M[0, 2] = M[0,2] + pose_img[0]
-            M[1, 2] = M[1,2] + pose_img[1] - 133
+            M[1, 2] = M[1,2] + pose_img[1] - rot_y
 
             # print M
 
@@ -61,13 +62,13 @@ class map:
             reshape = (img.shape[1], img.shape[0])
             img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
             img = cv2.warpPerspective(img, H, self._dim, borderValue = -1) # img in the robot frame
-            img = cv2.resize(img, reshape, interpolation = cv2.INTER_AREA)
+            #img = cv2.resize(img, reshape, interpolation = cv2.INTER_AREA)
             img = cv2.warpAffine(img, M, self._dim, borderValue = -1) # img in the world frame
 
             reshape = (mask.shape[1], mask.shape[0])
             mask = cv2.cvtColor(mask, cv2.COLOR_RGB2GRAY)
             mask = cv2.warpPerspective(mask, H, self._dim) # img in the robot frame
-            mask = cv2.resize(mask, reshape, interpolation = cv2.INTER_AREA)
+            #mask = cv2.resize(mask, reshape, interpolation = cv2.INTER_CUBIC)
             mask = cv2.warpAffine(mask, M, self._dim) # img in the world frame
             mask_xy = np.argwhere(mask > 0)
 
@@ -78,12 +79,11 @@ class map:
             # img should be downsampled in a way we can add squares to the grid
 
             for x, y in mask_xy:
-                #g_x = int(pose_img[1] + x - 133)
-                #g_y = int(pose_img[0] + y)
                 g_x = x
                 g_y = y
-                if 0 < g_x and g_x < self._dim[0] and 0 < g_y and g_y < self._dim[1]:
+                if 0 <= g_x and g_x < self._dim[0] and 0 <= g_y and g_y < self._dim[1]:
                     tmp[g_x][g_y] = img[x][y]/255.0 #self._grid[g_x][g_y] + 0.1
+                    #tmp[g_x][g_y] = mask[x][y]
 
             if self.total_img is None:
                 self.total_img = tmp
@@ -95,50 +95,17 @@ class map:
             # temp = self._grid + resized_image
             # temp = cv2.addWeighted(self._grid, 0.5, resized_image,0.5,0)
 
-            render = self.total_img / self.count
-            cv2.circle(render, (pose_img[0], pose_img[1]), 4, (255,255,255), -1)
+            render = self.total_img / (self.count + 0.00001)
+            cv2.circle(render, (pose_img[0], pose_img[1]), 4, color=(235,20,20), thickness=-1)
 
             cv2.imshow("warped img", render)
             cv2.waitKey(1)
-            time.sleep(0.1)
+            #time.sleep(0.01)
         # cv2.waitKey(0)
-        plt.show()
+        #plt.show()
 
-    def get_M(self, pose):
-        x = 0*pose[0]
-        y = 0*pose[1]
-        angle = pose[2]
-
-        s = np.sin(angle)
-        c = np.cos(angle)
-        M = np.array([[c, -s, x], [s, c, y]], float)
-
-        return M
-
-    def get_motion(self, delta_pos):
-        """Get the matrix that shifts coordinates between two poses
-           related by delta_pos = (delta_distance, delta_angle)."""
-
-        # print delta_pos
-        dist = delta_pos[0]
-        angle = np.deg2rad(delta_pos[1])
-
-        s = np.sin(angle)
-        c = np.cos(angle)
-        dx = dist*c
-        dy = dist*s
-
-        M = self.get_M(np.array([dx, dy, angle]))
-
-        # adjust for the centre of rotation. TODO: compensate for camera movement?
-        cr = np.array([self._pose[0] + self._res[0]/2, self._pose[1] + self._res[1]/2])
-        R  = M[:2,:2]
-        tc = R.dot(cr) - cr
-        M[0,2] -= tc[0]
-        M[1,2] -= tc[1]
-
-        return M
-
+        img = self.total_img / (self.count + 1e-10)
+        print cv2.imwrite('out.png', img*255)
 
 def parse_obs(file_dir, res):
     data = open(file_dir)
@@ -153,10 +120,11 @@ def parse_obs(file_dir, res):
     return obs
 
 if __name__ == "__main__":
-    dim = (500, 500)
-    resolution = 1 # mm^2 per cell
+    dim = (1000, 1000)
+    resolution = .5 # cm per pixel
 
     observations = parse_obs(img_dir + "/data_log.txt", resolution)
     m = map(dim, resolution)
     m.update_map(observations)
-    m.show_grid()
+    #m.show_grid()
+    raw_input()
